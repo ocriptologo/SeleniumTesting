b6c41b566b68f4478b5530d6720aa80e99782442
ocriptologo <113944723+ocriptologo@users.noreply.github.com>
2022-12-13 22:50:29 -0300

    Teste2

--- comita.sh
+++ comita.sh
@@ -1,12 +1,22 @@
-echo "Escreva a mensagem de push"
-read message
-git add .
-git commit -m"${message}"
-if [ -n "$(git status - porcelain)" ];
-then
- echo "Está limpo."
+echo "Considerando os resultados, você ainda quer realizar o commit e o push? (sim/nao)"
+read n
+yes=$(echo $n | tr -s '[:upper:]' '[:lower:]')
+if [[  "$n" = "sim"  ]] ; then
+  echo "My name"
+  echo "Escreva a mensagem de commit"
+        read message
+        git add .
+        git commit -m"${message}"
+        if [ -n "$(git status - porcelain)" ];
+            then
+                echo "Está limpo."
+            else
+                git status
+            echo "Fazendo o push!!"
+            git push -u origin master
+            echo "Feito!"
+        fi
 else
- git status
- echo "Fazendo o push!!"
- git push -u origin master
+  echo "Ok, até mais!"
 fi
+exit
